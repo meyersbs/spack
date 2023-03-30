fe4dd0d7ae5aa3f444cb48b5710fd3a34a363d35
Benjamin Meyers <bsmits@rit.edu>
2023-03-30 10:41:36 -0400

    New package py-exceptiongroup

--- var/spack/repos/builtin/packages/py-exceptiongroup/package.py
+++ var/spack/repos/builtin/packages/py-exceptiongroup/package.py
@@ -3,17 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack.package import *
 
 
 class PyExceptiongroup(PythonPackage):
-    """A backport of the BaseExceptionGroup and ExceptionGroup classes from Python 3.11."""
+    """This is a backport of the BaseExceptionGroup and ExceptionGroup classes from
+    Python 3.11."""
 
     homepage = "https://github.com/agronholm/exceptiongroup"
-    pypi = "exceptiongroup/exceptiongroup-1.0.4.tar.gz"
+    pypi     = "exceptiongroup/exceptiongroup-1.1.0.tar.gz"
 
-    version("1.0.4", sha256="bd14967b79cd9bdb54d97323216f8fdf533e278df937aa2a90089e7d6e06e5ec")
+    maintainers("meyersbs")
 
-    depends_on("python@3.7:", type=("build", "run"))
+    version("1.1.0", sha256="bcb67d800a4497e1b404c2dd44fca47d3b7a5e5433dbab67f96c1a685cdfdf23")
+
     depends_on("py-flit-scm", type="build")
+    depends_on("python@3.7:", type=("build", "run"))
